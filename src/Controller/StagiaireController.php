7de688451f59391b7769568b3b389d9e2a63694e
KrouriB <brice.krouri@gmail.com>
2023-07-21 14:19:56 +0200

    envoie de resultat des vue lie stagiaire

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\StagiaireRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StagiaireController extends AbstractController
 {
     #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(): Response
+    public function index(StagiaireRepository $stagiaireRepository): Response
     {
+        $stagiaires = $stagiaireRepository->findBy([],['nom' => 'ASC']);
         return $this->render('stagiaire/index.html.twig', [
-            'controller_name' => 'StagiaireController',
+            'stagiaires' => $stagiaires
+        ]);
+    }
+
+    #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
+    public function show(Stagiaire $stagiaire): Response
+    {
+        return $this->render('stagiaire/show.html.twig', [
+            'stagiaire' => $stagiaire
         ]);
     }
 }
