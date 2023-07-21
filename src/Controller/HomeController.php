61fd39c5509ae28d48f0428a5d296992dc0809e5
KrouriB <brice.krouri@gmail.com>
2023-07-21 14:17:52 +0200

    pour affichage des session sur homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SessionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(SessionRepository $sessionRepository): Response
     {
+        $past = $sessionRepository->findPastSessionHomePage();
+        $now = $sessionRepository->findCurrentSessionHomePage();
+        $futur = $sessionRepository->findFuturSessionHomePage();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'past' => $past,
+            'now' => $now,
+            'futur' => $futur
         ]);
     }
 }
