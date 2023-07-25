44896f219faff6a5a7d79fea343212c40fa7f47b
KrouriB <brice.krouri@gmail.com>
2023-07-25 16:35:16 +0200

    ajout add/del stag/prog, add stag a modifié

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Session;
+use App\Entity\Stagiaire;
 use App\Form\SessionType;
 use App\Repository\SessionRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -67,11 +68,65 @@ class SessionController extends AbstractController
         ]);
     }
 
-    #[Route('/session'/*/{id}'*/, name: 'app_session')]
-    public function index(/*Session $session*/): Response
+    #[Route('/session/{id}', name: 'app_session')]
+    public function index(Session $session): Response
     {
+        $stagiaires = $stagiaireRepository->findBy([],['nom' => 'ASC']);
+        $modules = $moduleRepository->findBy([],['nom' => 'ASC']);
         return $this->render('session/index.html.twig', [
-            /*formuaire programme + ajout stagiaire*/
+            'session' => $session,
+            'stagiaires' => $stagiaires,
+            'modules' => $modules
         ]);
+    }
+
+    #[Route('/session/{id}/addStagiaire', name: 'add_stagiaire')]
+    public function addStagiaire(Session $session, Stagiaire $stagiaire, EntityManagerInterface $entityManager): Response
+    {
+        $session->addStagiaire($stagiaire);
+
+        $entityManager->persist($stagiaire);
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('app_session', ['id' => $session->getId()]);
+    }
+
+    #[Route('/session/{id}/deleteStagiaire', name: 'delete_stagiaire')]
+    public function deleteStagiaire(Session $session, Stagiaire $stagiaire, EntityManagerInterface $entityManager): Response
+    {
+        $session->removeStagiaire($stagiaire);
+
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('app_session', ['id' => $session->getId()]);
+    }
+
+    #[Route('/session/{id}/addModule', name: 'add_module')]
+    public function addModule(Session $session, Module $module, int $nbJours, EntityManagerInterface $entityManager): Response
+    {
+        $programme = new Programme();
+        $programme->setNombreJours($nbJours);
+
+        $module->addProgramme($programme);
+
+        $session->addProgramme($programme);
+
+        $entityManager->persist($programme);
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('app_session', ['id' => $session->getId()]);
+    }
+
+    #[Route('/session/{id}/deleteModule', name: 'delete_module')]
+    public function deleteModule(Session $session, Programme $programme, EntityManagerInterface $entityManager): Response
+    {
+        $programme-getModule()->removeProgramme($programme);
+
+        $session->removeProgramme($programme);
+
+        $entityManager->remove($programme);
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('app_session', ['id' => $session->getId()]);
     }
 }
