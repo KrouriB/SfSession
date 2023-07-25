530c53970246e596eebf97386aa4793483da168b
KrouriB <brice.krouri@gmail.com>
2023-07-25 11:18:14 +0200

    login homepage - register registerpage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,26 +4,45 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\HomeType;
-use App\Repository\UserRepository;
+use App\Form\HomeFormType;
 use App\Security\EmailVerifier;
+use App\Repository\UserRepository;
+use Symfony\Component\Mime\Address;
+use App\Repository\SessionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use App\Repository\SessionRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class HomeController extends AbstractController
 {
 
     #[Route('/home', name: 'app_home')]
-    public function index(AuthenticationUtils $authenticationUtils, Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SessionRepository $sessionRepository): Response
+    public function index(SessionRepository $sessionRepository): Response
+    {
+        
+        
+        $past = $sessionRepository->findPastSessionHomePage();
+        $now = $sessionRepository->findCurrentSessionHomePage();
+        $futur = $sessionRepository->findNextSessionHomePage();
+        
+        return $this->render('home/index.html.twig', [
+            'past' => $past,
+            'now' => $now,
+            'futur' => $futur
+        ]);
+    }
+
+    
+
+    #[Route('/loginTemplate', name: 'app_login_template')]
+    public function loginRoute(AuthenticationUtils $authenticationUtils)
     {
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
@@ -32,44 +51,9 @@ class HomeController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-        $user = new User();
-        $form = $this->createForm(HomeType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('verification@admin.com', 'Verif'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-            // do anything else you need here, like send an email
-
-            return $this->redirectToRoute('app_home');
-        }
-        $past = $sessionRepository->findPastSessionHomePage();
-        $now = $sessionRepository->findCurrentSessionHomePage();
-        $futur = $sessionRepository->findNextSessionHomePage();
-        return $this->render('home/index.html.twig', [
-            'past' => $past,
-            'now' => $now,
-            'futur' => $futur,
+        return $this->render('home/login.html.twig', [
             'last_username' => $lastUsername,
-            'error' => $error,
-            'registrationForm' => $form->createView()
+            'error' => $error
         ]);
     }
 
