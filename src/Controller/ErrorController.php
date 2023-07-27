989647a723d32bdb812f97dc82a2f1142d878340
KrouriB <brice.krouri@gmail.com>
2023-07-27 15:10:56 +0200

    error 404 page

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ErrorController extends AbstractController
+{
+    #[Route('/error404', name: 'error_404')]
+    public function error404(): Response
+    {
+        return $this->render('error/404.html.twig', []);
+    }
+
+    #[Route('/error', name: 'app_error')]
+    public function index(): Response
+    {
+        return $this->render('error/index.html.twig', [
+            'controller_name' => 'ErrorController',
+        ]);
+    }
+}
