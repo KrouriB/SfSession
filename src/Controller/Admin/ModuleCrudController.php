4c956f2b14d8895bcc31eba9788f11c0e538142d
KrouriB <brice.krouri@gmail.com>
2023-08-21 16:19:29 +0200

    modif on crud

--- src/Controller/Admin/ModuleCrudController.php
+++ src/Controller/Admin/ModuleCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Module;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ModuleCrudController extends AbstractCrudController
@@ -14,14 +17,15 @@ class ModuleCrudController extends AbstractCrudController
         return Module::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('nom'),
+            TextField::new('categorie')
+                ->onlyOnIndex(),
+            AssociationField::new('categorie')
+                ->hideOnIndex(),
         ];
     }
-    */
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -14,14 +18,14 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('pseudo'),
+            EmailField::new('email'),
+            BooleanField::new('isVerified')->renderAsSwitch(false)->onlyOnIndex(),
+            BooleanField::new('isVerified')->hideOnIndex(),
         ];
     }
-    */
 }
