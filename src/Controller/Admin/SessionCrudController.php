aa301af8243f0843c4b8e21de3918c90dafaded0
KrouriB <brice.krouri@gmail.com>
2023-08-21 15:27:18 +0200

    crud de session (pas entierement fonctonnel)

--- src/Controller/Admin/SessionCrudController.php
+++ src/Controller/Admin/SessionCrudController.php
@@ -4,8 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Session;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class SessionCrudController extends AbstractCrudController
@@ -17,14 +22,24 @@ class SessionCrudController extends AbstractCrudController
         return Session::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+                ->onlyOnIndex(),
+            TextField::new('nom'),
+            DateField::new('dateDebut'),
+            DateField::new('dateFin'),
+            NumberField::new('nombrePlaceTheorique'),
+            AssociationField::new('stagiaires'),
+            AssociationField::new('programmes')
+                ->onlyOnIndex(),
+            ArrayField::new('programmes')
+                ->hideOnIndex(),
+            TextField::new('formation')
+                ->onlyOnIndex(),
+            AssociationField::new('formation')
+                ->hideOnIndex(),
         ];
     }
-    */
 }
