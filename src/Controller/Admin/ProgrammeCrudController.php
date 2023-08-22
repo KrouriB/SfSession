d0476bed659f919d895f0a4464f69ebe1b9d09a2
KrouriB <brice.krouri@gmail.com>
2023-08-22 17:00:03 +0200

    programme crud

--- src/Controller/Admin/ProgrammeCrudController.php
+++ src/Controller/Admin/ProgrammeCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Programme;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ProgrammeCrudController extends AbstractCrudController
@@ -13,14 +17,15 @@ class ProgrammeCrudController extends AbstractCrudController
         return Programme::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('module')->onlyOnIndex(),
+            AssociationField::new('module')->hideOnIndex(),
+            TextField::new('session')->onlyOnIndex(),
+            AssociationField::new('session')->hideOnIndex(),
+            NumberField::new('nombreJours'),
         ];
     }
-    */
 }
