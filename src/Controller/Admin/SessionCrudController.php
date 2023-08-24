09042c8952a7e98725781800b518f696842b6539
KrouriB <brice.krouri@gmail.com>
2023-08-24 14:26:52 +0200

    cleaning

--- src/Controller/Admin/SessionCrudController.php
+++ src/Controller/Admin/SessionCrudController.php
@@ -2,87 +2,24 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Module;
 use App\Entity\Session;
 use App\Entity\Programme;
 use App\Form\ProgrammeType;
-use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\HttpFoundation\Response;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use Symfony\Component\Form\FormBuilderInterface;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\FieldDto;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\AssetsDto;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RequestStack;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Option\EA;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
-use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityUpdater;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\BatchActionDto;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Factory\FormFactory;
-
-use EasyCorp\Bundle\EasyAdminBundle\Security\Permission;
-use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
-use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
-use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityRepository;
-use EasyCorp\Bundle\EasyAdminBundle\Factory\ActionFactory;
-use EasyCorp\Bundle\EasyAdminBundle\Factory\EntityFactory;
-use EasyCorp\Bundle\EasyAdminBundle\Factory\FilterFactory;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Provider\FieldProvider;
-use EasyCorp\Bundle\EasyAdminBundle\Factory\PaginatorFactory;
-use EasyCorp\Bundle\EasyAdminBundle\Form\Type\FileUploadType;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
-use EasyCorp\Bundle\EasyAdminBundle\Factory\ControllerFactory;
-use EasyCorp\Bundle\EasyAdminBundle\Form\Type\FiltersFormType;
-use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
-use EasyCorp\Bundle\EasyAdminBundle\Event\AfterCrudActionEvent;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
-use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeCrudActionEvent;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityDeletedEvent;
-use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityUpdatedEvent;
-use EasyCorp\Bundle\EasyAdminBundle\Provider\AdminContextProvider;
-use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityDeletedEvent;
-use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityPersistedEvent;
-use EasyCorp\Bundle\EasyAdminBundle\Exception\EntityRemoveException;
-use EasyCorp\Bundle\EasyAdminBundle\Form\Type\Model\FileUploadState;
-use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Exception\ForbiddenActionException;
-use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
-use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
-use EasyCorp\Bundle\EasyAdminBundle\Contracts\Controller\CrudControllerInterface;
-use EasyCorp\Bundle\EasyAdminBundle\Exception\InsufficientEntityPermissionException;use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
+
+
 
 class SessionCrudController extends AbstractCrudController
 {
-    // private $requestStack;
-
-    // public function __construct(RequestStack $requestStack)
-    // {
-    //     $this->requestStack = $requestStack;
-    // }
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->em = $entityManager;
@@ -95,9 +32,6 @@ class SessionCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        // // Récupérez l'ID de la session à partir de la requête en cours
-        // $request = $this->requestStack->getCurrentRequest();
-        // $sessionId = $request->query->get('entityId'); // 'entityId' doit correspondre au nom de l'identifiant dans votre configuration EasyAdmin
 
         return [
             IdField::new('id')
@@ -119,117 +53,16 @@ class SessionCrudController extends AbstractCrudController
             AssociationField::new('programmes')
                 ->onlyOnIndex(),
             CollectionField::new('programmes')
+                ->onlyOnForms()
                 ->setEntryIsComplex(true) // Indique que chaque entrée est complexe (Module + nombre de jours)
                 ->setFormTypeOptions([
                     // 'entry_type' => ProgrammeType::class, // Le formulaire pour chaque entrée
                     'allow_add' => true, // Autoriser l'ajout de nouvelles entrées
                     'allow_delete' => true, // Autoriser la suppression d'entrées existantes
                     'by_reference' => false, // Nécessaire pour que les modifications soient persistées correctement
-                    // 'session_id' => $sessionId, // Transmettez l'ID de la session à ProgrammeType
                 ])
-                ->setEntryType(ProgrammeType::class) // Le formulaire pour chaque entrée
-                // ->setEntryOptions([
-                //     'session_id' => 1, // Transmettez l'ID de la session à ProgrammeType via entry_options
-                // ])
-                ->hideOnIndex(),
+                ->setEntryType(ProgrammeType::class), // Le formulaire pour chaque entrée
         ];
     }
-
-
-    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
-    {
-        dd($entityInstance);
-        $entityManager->persist($entityInstance);
-        $entityManager->flush();
-    }
-
-    public function edit(AdminContext $context)
-    {
-        $event = new BeforeCrudActionEvent($context);
-        $this->container->get('event_dispatcher')->dispatch($event);
-        if ($event->isPropagationStopped()) {
-            return $event->getResponse();
-        }
-        
-        if (!$this->isGranted(Permission::EA_EXECUTE_ACTION, ['action' => Action::EDIT, 'entity' => $context->getEntity()])) {
-            throw new ForbiddenActionException($context);
-        }
-        
-        if (!$context->getEntity()->isAccessible()) {
-            throw new InsufficientEntityPermissionException($context);
-        }
-
-        $this->container->get(EntityFactory::class)->processFields($context->getEntity(), FieldCollection::new($this->configureFields(Crud::PAGE_EDIT)));
-        $context->getCrud()->setFieldAssets($this->getFieldAssets($context->getEntity()->getFields()));
-        $this->container->get(EntityFactory::class)->processActions($context->getEntity(), $context->getCrud()->getActionsConfig());
-        $entityInstance = $context->getEntity()->getInstance();
-        if ($context->getRequest()->isXmlHttpRequest()) {
-            if ('PATCH' !== $context->getRequest()->getMethod()) {
-                throw new MethodNotAllowedHttpException(['PATCH']);
-            }
-            
-            if (!$this->isCsrfTokenValid(BooleanField::CSRF_TOKEN_NAME, $context->getRequest()->query->get('csrfToken'))) {
-                if (class_exists(InvalidCsrfTokenException::class)) {
-                    throw new InvalidCsrfTokenException();
-                } else {
-                    return new Response('Invalid CSRF token.', 400);
-                }
-            }
-
-            $fieldName = $context->getRequest()->query->get('fieldName');
-            $newValue = 'true' === mb_strtolower($context->getRequest()->query->get('newValue'));
-            
-
-            try {
-                $event = $this->ajaxEdit($context->getEntity(), $fieldName, $newValue);
-            } catch (\Exception) {
-                throw new BadRequestHttpException();
-            }
-
-            if ($event->isPropagationStopped()) {
-                return $event->getResponse();
-            }
-
-            return new Response($newValue ? '1' : '0');
-        }
-        $editForm = $this->createEditForm($context->getEntity(), $context->getCrud()->getEditFormOptions(), $context);
-        // dd($editForm->handleRequest($context->getRequest()));
-        // $sessionData = $context->getRequest()->request->get('Session');
-            
-        $programme = New Programme();
-        $programme->setModule($this->em->getRepository(Module::class)->find($context->getRequest()->request->all('Session')["programmes"][1]['module']));
-        $programme->setSession($this->em->getRepository(Session::class)->find($context->getRequest()->request->all('Session')["programmes"][1]['session']));
-        $programme->setNombreJours($context->getRequest()->request->all('Session')["programmes"][1]['nombreJours']);
-        dd($programme);
-        // dd($context->getRequest()->request->all());
-        $editForm->handleRequest($context->getRequest());
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->processUploadedFiles($editForm);
-            
-            $event = new BeforeEntityUpdatedEvent($entityInstance);
-            $this->container->get('event_dispatcher')->dispatch($event);
-            $entityInstance = $event->getEntityInstance();
-            
-            $this->updateEntity($this->container->get('doctrine')->getManagerForClass($context->getEntity()->getFqcn()), $entityInstance);
-
-            $this->container->get('event_dispatcher')->dispatch(new AfterEntityUpdatedEvent($entityInstance));
-
-            return $this->getRedirectResponseAfterSave($context, Action::EDIT);
-        }
-
-        $responseParameters = $this->configureResponseParameters(KeyValueStore::new([
-            'pageName' => Crud::PAGE_EDIT,
-            'templateName' => 'crud/edit',
-            'edit_form' => $editForm,
-            'entity' => $context->getEntity(),
-        ]));
-
-        $event = new AfterCrudActionEvent($context, $responseParameters);
-        $this->container->get('event_dispatcher')->dispatch($event);
-        if ($event->isPropagationStopped()) {
-            return $event->getResponse();
-        }
-
-        return $responseParameters;
-    }
+    
 }
