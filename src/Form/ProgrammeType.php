b246873dfe8fd9a25a860a6694c8b35fc4d4fce2
KrouriB <brice.krouri@gmail.com>
2023-08-24 14:24:15 +0200

    modif pour correspondre a l'affichage souhaiter

--- src/Form/ProgrammeType.php
+++ src/Form/ProgrammeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Module;
 use App\Entity\Session;
+use App\Entity\Programme;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -12,7 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class ProgrammeType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         // $sessionId = $options['session_id'];
 
@@ -25,20 +26,19 @@ class ProgrammeType extends AbstractType
             ])
             ->add('session', EntityType::class, [
                 'class' => Session::class,
-                'choice_label' => 'nom', // Affiche le champ 'nom' de l'entité Module
+                'choice_label' => 'nom', // Affiche le champ 'nom' de l'entité Session
                 'label' => 'Session',
-                'placeholder' => 'Sélectionnez un session', // Optionnel : affiche un libellé de sélection
+                'row_attr' => ['style' => 'display:none;'], // En mode lecture seule
             ])
             ->add('nombreJours', IntegerType::class, [
                 'label' => 'Nombre de Jours',
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configurez ici les options du formulaire, si nécessaire.
-            //'data_class' => 'App\Entity\Programme',
+            'data_class' => Programme::class,
         ]);
     }
 
